2267b5f6515e21b548cdda43050179753778b9bf
Matheus <math2306.r@gmail.com>
2024-02-07 14:41:24 -0300

    teste

--- 02-aritimeticos.js
+++ 02-aritimeticos.js
@@ -20,7 +20,58 @@
 //regras para nomear identificadores (var, const ou let)
 // podem comçar com A-Z, a-z, _ ou $
 
-var MinhaVariavel = 10;
-var outraVariavel = 20;
-const _minhavariavel = 3.1514;
-var $minhavariavel = 11;
+// var MinhaVariavel = 10;
+// var outraVariavel = 20;
+// const _minhavariavel = 3.1514;
+// var $minhavariavel = 11;
+
+// var v1 = 10;
+// var v2 = 3;
+// console.log("-----------------------------");
+// console.log("Valores v1=", v1, "| v2 =", v2);
+// console.log("-----------------------------");
+// console.log("resto de divisao inteira v1 % v2=", v1 ** v2);
+// console.log("exponenciacao v1**v2 (v1 elevado a v2)=", v1 ** v2);
+
+//exercicios de fixacao
+var pessoa = "marcos";
+var banco = "Inter";
+var agencia = 45;
+var conta = 993456;
+var saldo = 7594;
+
+console.log(
+  "seja bem vindo",
+  pessoa,
+  "proprietario da conta:",
+  conta,
+  "a agencia",
+  agencia,
+  "do banco do",
+  banco,
+  "seu saldo atual é de R$",
+  saldo
+);
+console.log("-------------------------------------------------------");
+console.log(
+  "detectamos uma atividade de sua conta de compra no valor de R$300"
+);
+console.log("seu saldo atual é de", saldo - 300);
+saldo=saldo-300
+console.log("-------------------------------------------------------")
+console.log("foi feito uma transferencia via pix no valor de 200")
+console.log("saldo de:", saldo + 200)
+saldo=saldo + 200
+console.log("-------------------------------------------------------")
+console.log("foi feito uma transferencia via pix no valor de 90")
+console.log("saldo de:", saldo + 90)
+console.log("-------------------------------------------------------")
+saldo= saldo +90
+console.log("compra no valor de 40")
+console.log("saldo de:", saldo - 40)
+console.log("-------------------------------------------------------")
+saldo= saldo-90
+console.log("compra no valor de 500")
+console.log("saldo de:", saldo - 500)
+
+
